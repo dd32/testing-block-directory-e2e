b24dbe0a21db590c1c0d28fd5cd43ed4761e9d06
dufresnesteven <steve.dufresne@automattic.com>
2020-06-18 11:42:40 +0900

    Try running more tests.

--- specs/index.js
+++ specs/index.js
@@ -1,14 +1,28 @@
 /**
  * WordPress dependencies
  */
-import { createNewPost } from '@wordpress/e2e-test-utils';
+import {
+	createNewPost,
+	insertBlock,
+	getAllBlocks,
+} from '@wordpress/e2e-test-utils';
 
-describe( 'Block Directory Test', () => {
+describe( 'Block Directory Tests', () => {
 	beforeAll( async () => {
 		await createNewPost();
 	} );
 
-	it( 'try this test', async () => {
-		expect(false).toBeFalsy();
+	beforeEach( async () => {
+		// Remove all blocks from the post so that we're working with a clean slate
+		await page.evaluate( () => {
+			const blocks = wp.data.select( 'core/block-editor' ).getBlocks();
+			const clientIds = blocks.map( ( block ) => block.clientId );
+			wp.data.dispatch( 'core/block-editor' ).removeBlocks( clientIds );
+		} );
+	} );
+
+	it( 'Block can be inserted in the document', async () => {
+		await insertBlock( process.env.BLOCK_NAME );
+		expect( await getAllBlocks() ).toHaveLength( 1 );
 	} );
 } );
