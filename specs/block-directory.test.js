a65355e49262507bd7a6c5fe55421ee1e4b80219
dufresnesteven <steve.dufresne@automattic.com>
2020-07-09 13:00:59 +0900

    Combine tests.

--- specs/block-directory.test.js
+++ specs/block-directory.test.js
@@ -13,13 +13,20 @@ import {
 	getEditedPostContent,
 	deactivatePlugin,
 	uninstallPlugin,
+	insertBlock,
+	getAllBlocks,
 } from '@wordpress/e2e-test-utils';
 
 /**
  * Internal dependencies
  */
 
-import { getInstalledBlocks, runTest, removeAllBlocks } from '../utils';
+import {
+	getInstalledBlocks,
+	getThirdPartyBlocks,
+	runTest,
+	removeAllBlocks,
+} from '../utils';
 
 // We don't want to see warnings during these tests
 console.warn = () => {};
@@ -30,11 +37,21 @@ describe( `Block Directory Tests`, () => {
 		await removeAllBlocks();
 	} );
 
+	// afterAll( async () => {
+	// 	const [ block ] = await getThirdPartyBlocks();
+
+	// 	console.log( 'Deactivating: ', block.id );
+	// 	await deactivatePlugin( block.id );
+
+	// 	console.log( 'Uninstalling: ', block.id );
+	// 	await uninstallPlugin( block.id );
+	// } );
+
 	it( 'Block returns from API and installs', async () => {
 		try {
-			const { searchTerm } = github.context.payload.client_payload || 
-
-            console.log( 'Running a search for: ', searchTerm );
+			//const { searchTerm } = github.context.payload.client_payload ||
+			const searchTerm = 'p5';
+			console.log( 'Running a search for: ', searchTerm );
 			// Search for the block via the inserter
 			await searchForBlock( searchTerm );
 
@@ -56,6 +73,29 @@ describe( `Block Directory Tests`, () => {
 			runTest( () => {
 				expect( content.length ).toBeGreaterThan( 0 );
 			}, "Couldn't install the block." );
+		} catch ( e ) {
+			core.setFailed( e );
+		}
+	} );
+
+	it( 'Block can be inserted in the document', async () => {
+		try {
+			const blocks = await getThirdPartyBlocks();
+
+			runTest( () => {
+				expect( blocks.length ).toBeGreaterThan( 0 );
+			}, 'Could not find block in registered block list.' );
+
+			runTest( () => {
+				expect( blocks ).toHaveLength( 1 );
+			}, 'Block registers multiple blocks.' );
+
+			await insertBlock( blocks[ 0 ] );
+			const blockList = await getAllBlocks();
+
+			runTest( () => {
+				expect( blockList ).toHaveLength( 1 );
+			}, 'Block was not found in document after insert.' );
 		} catch ( e ) {
 			core.setFailed( e );
 		}
