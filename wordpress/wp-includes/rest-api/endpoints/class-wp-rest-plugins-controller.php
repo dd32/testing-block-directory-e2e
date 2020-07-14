76a307d7557adcc2f93b2786ad3a3e60e219c630
dufresnesteven <steve.dufresne@automattic.com>
2020-07-14 12:02:34 +0900

    Print skin error.

--- wordpress/wp-includes/rest-api/endpoints/class-wp-rest-plugins-controller.php
+++ wordpress/wp-includes/rest-api/endpoints/class-wp-rest-plugins-controller.php
@@ -332,9 +332,12 @@ class WP_REST_Plugins_Controller extends WP_REST_Controller {
 		}
 		error_log( 'Checking skin errors' );
 		if ( $skin->get_errors()->has_errors() ) {
+			error_log( 'We have skin errors!' );
+
+			error_log( print_r( $skin, true) );
 			$error = $skin->get_errors();
 			$error->add_data( array( 'status' => 500 ) );
-
+			
 			return $error;
 		}
 
